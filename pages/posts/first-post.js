1636881f6a1b7ca6cd76b7f025cbc87f9bc6de4a
Dilraj <dsomel21@gmail.com>
2022-01-15 20:39:51 -0500

    Getting to CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,25 @@
 import Link from 'next/link';
-
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      {/* <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        strategy='lazyOnload'
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
       <h1>First Post</h1>
       Back to{' '}
       <Link href='/'>
         <a>Home</a>
       </Link>
-    </>
+    </Layout>
   );
 }
